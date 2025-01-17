8c013b6f50c383a73de2083f0c03add34bb494a7
JOHNNY GUILLERMO GREFA CALAPUCHA <jg.grefac@uea.edu.ec>
2025-01-17 14:00:46 -0500

    subir

--- Semana_6_ejercicio2/obj/Debug/net9.0/Semana_6_ejercicio2.AssemblyInfo.cs
+++ Semana_6_ejercicio2/obj/Debug/net9.0/Semana_6_ejercicio2.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Semana_7_ejercicio2")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Semana_6_ejercicio2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+909e5977dac76ff589a8554174007a0506f552b2")]
-[assembly: System.Reflection.AssemblyProductAttribute("Semana_7_ejercicio2")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Semana_7_ejercicio2")]
+[assembly: System.Reflection.AssemblyProductAttribute("Semana_6_ejercicio2")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Semana_6_ejercicio2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
